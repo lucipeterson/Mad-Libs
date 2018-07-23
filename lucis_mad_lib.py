6c899f35bfd981555526237e949199906e15e0a8
Luci Peterson <lucijpeterson@gmail.com>
2018-07-22 22:36:01 -0700

    Initial commit

---
+++ lucis_mad_lib.py
@@ -0,0 +1,11 @@
+#lucis_mad_lib.py
+print("~~ LUCI'S MAD LIBS ~~\nForrest Gump Edition\n")
+propernoun = input("Please enter a noun: ")
+verb = input("Type a past tense verb: ")
+noun = input("Another noun: ")
+noun2 = input("And another noun please: ")
+pluralnoun = input("Give me a plural noun: ")
+verb2 = input("Give me another verb: ")
+verb3 = input("One last verb: ")
+quote = "My " + propernoun + " always " + verb + " " + noun + " is like a " + noun2 + " of " + pluralnoun + "." + " You never " + verb2 + " what you're gonna " + verb3 + "."
+print(quote)
